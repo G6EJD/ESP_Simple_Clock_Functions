a8a4312d5359cac8ede2aaec384029b82ac7647d
G6EJD <d.l.bird@sky.com>
2019-05-19 11:27:07 +0100

    Update ESP_RTC_with_NTP_Synchronisation_SH1106_OLED.ino

--- ESP_RTC_with_NTP_Synchronisation_SH1106_OLED.ino
+++ ESP_RTC_with_NTP_Synchronisation_SH1106_OLED.ino
@@ -9,6 +9,7 @@
 #endif
 
 #include "time.h"
+#include <Wire.h>
 #include "SH1106.h"               // See https://github.com/squix78/esp8266-oled-ssd1306 or via Sketch/Include Library/Manage Libraries
 SH1106 display(0x3c, SDA, SCL);   // OLED display object definition (address, SDA, SCL)
 
@@ -35,7 +36,11 @@ volatile unsigned int update_duration = 60*60; // Time duration in seconds, so s
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin(SDA, SCL,100000);        // (sda,scl,bus_speed) Start the Wire service for the OLED display using assigned pins for SCL and SDA
+  #if ESP8266
+  Wire.begin(SDA, SCL);               // (sda,scl,bus_speed) Start the Wire service for the OLED display using pin=D4 for SCL and Pin-D3 for SDA
+  #else
+  Wire.begin(SDA, SCL, 100000);       // (sda,scl,bus_speed) Start the Wire service for the OLED display using pin=D4 for SCL and Pin-D3 for SDA
+  #endif  
   display.init();                     // Initialise the display
   display.flipScreenVertically();     // In my case flip the screen around by 180°
   display.setContrast(255);           // If you want turn the display contrast down, 255 is maxium and 0 in minimum, in practice about 128 is OK
